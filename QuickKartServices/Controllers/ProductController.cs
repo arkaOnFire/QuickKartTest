39a5714d34d4d486893706c26dc10b4f7d6b12f4
Arka Ghosal <arka.ghosal@live.com>
2024-11-01 16:35:10 +0530

    First Commit

---
+++ QuickKartServices/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuickKartDataAccessLayer;
+using QuickKartDataAccessLayer.Models;
+
+namespace QuickKartServices.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class ProductController : Controller
+	{
+		QuickKartRepository repository;
+		public ProductController(QuickKartRepository repository)
+		{
+			this.repository = repository;
+		}
+		[HttpGet]
+		public JsonResult GetAllProducts()
+		{
+			List<Product> products = new List<Product>();
+			try
+			{
+				products = repository.GetAllProducts();
+			}
+			catch (Exception)
+			{
+				products = null;
+			}
+			return Json(products);
+		}
+
+
+	}
+}
---
+++ QuickKartServices/Program.cs
@@ -0,0 +1,35 @@
+using QuickKartDataAccessLayer.Models;
+using QuickKartDataAccessLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//DI to use QuickKartDBContext and QuickKartRepository in Controller
+builder.Services.AddTransient<QuickKartDBContext>();
+builder.Services.AddTransient<QuickKartRepository>(
+	c => new QuickKartRepository(c.GetRequiredService<QuickKartDBContext>()));
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
